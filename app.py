2cca89a4883ceb3afe606e6dec36edac334c1ff1
Mentalistv <82886983+Mentalistv@users.noreply.github.com>
2024-10-02 19:02:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+import re
+import streamlit as st
+import pickle
+
+def word_features(sentence, i, vocab):
+	word = sentence[i][0]
+	features = {
+		'word': word,
+		'is_first': i == 0, #if the word is a first word
+		'is_last': i == len(sentence) - 1, #if the word is a last word
+		'is_capitalized': word[0].upper() == word[0],
+		'is_all_caps': word.upper() == word,	 #word is in uppercase
+		'is_all_lower': word.lower() == word,	 #word is in lowercase
+		#prefix of the word
+		'prefix-1': word[0],
+		'prefix-2': word[:2],
+		'prefix-3': word[:3],
+		#suffix of the word
+		'suffix-1': word[-1],
+		'suffix-2': word[-2:],
+		'suffix-3': word[-3:],
+		#extracting previous word
+		'prev_word': '' if i == 0 else sentence[i-1][0],
+		#extracting next word
+		'next_word': '' if i == len(sentence)-1 else sentence[i+1][0],
+		'has_hyphen': '-' in word, #if word has hypen
+		'is_numeric': word.isdigit(), #if word is in numeric
+		'capitals_inside': word[1:].lower() != word[1:],
+        'is_unknown': word not in vocab  # Flag if the word is not in the known vocabulary - CHANGE
+	}
+	return features
+
+
+def tokenize(sentence):
+    tokens = re.findall(r"[\w]+|[.,!?;]", sentence)
+    return tokens
+
+def sent2features(sentence, vocab):
+    return [word_features(sentence, i, vocab) for i in range(len(sentence))]
+
+
+def predict(sentence, crf):
+    tokenized_sentence = tokenize(sentence)
+
+    sentence_with_placeholder_tags = [(word, 'O') for word in tokenized_sentence]
+
+    X_test_sentence = [sent2features(sentence_with_placeholder_tags, vocab)]
+
+    y_pred_sentence = crf.predict(X_test_sentence)
+    
+    return tokenized_sentence,y_pred_sentence[0]
+
+
+
+
+
+
+vocab = []
+with open("vocab.txt", 'r') as f:
+    vocab = [line.rstrip('\n') for line in f]
+
+crf_model = pickle.load(open("crf_model.pkl", 'rb')) 
+
+
+predict("Hi my name  is Harsh", crf_model)
+
+
+tag_list = ['DET', 'PRT', 'ADV', 'X', 'CONJ', 'ADJ', 'ADP', 'PRON', 'NOUN', '.', 'NUM', 'VERB']
+
+# Streamlit interface
+st.title("POS Tagging with CRF")
+
+# Input text
+sentence = st.text_area("Enter a sentence:")
+
+if st.button("Predict Tags"):
+    if sentence:
+        t,p = predict(sentence, crf_model)
+        st.write("Word : Tag")
+        for i in range(len(t)):
+            st.write(t[i],':',p[i])
